c4b3188c4b4f68bfca7a9c63a911b84b3dbb1e46
Asif Khan <akhan@codeinreach.com>
2019-07-21 20:11:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
         quantity.times do
             items << title
         end
-        @last_transaction_amount = price * quantity
+        @last_transaction_amount = price * quantity           
     end
 
     def apply_discount #instance method
